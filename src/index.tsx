e24d9649f016d9d1528b0a6686481c199453e1cd
Michael Bashurov <accounts@saitonakamura.me>
2021-04-26 19:36:22 +0300

    initial skeleton

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import "tailwindcss/tailwind.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>app</div>;
+
+ReactDOM.render(<App />, document.querySelector("#root"));
